f600d5b873237b98ad7600460f8c63df57e845a1
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-03-31 19:46:04 +0545

    for loop and if else

--- Operators/operation.py
+++ Operators/operation.py
@@ -1,4 +1,31 @@
+"""
 a = int(input("Enter a number: "));
 
 if a%2==0:
     print("A is even number")
+else:
+    print("A is odd number")
+"""
+
+"""b = int(input("Enter a number:"))
+if b<0:
+    print("B is negative number")
+elif b>0:
+    print("B is positive number")
+else:
+    print("B is zero")"""
+
+"""mark = int(input("Enter your mark: "))
+
+if mark>=90:
+    print("You got an A")
+elif mark>=70 and mark<=80:
+    print("You got a B")
+elif mark>=60 and mark<=70:
+    print("You got a C")
+else:
+    print("Sorry you failed!")"""
+
+for i in range(1,11):
+    print(i)
+
