32d0c92d0f8f01194848e194419c9b32483000c7
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-02 14:46:21 +0545

    add strings directory

---
+++ Strings/slicing.py
@@ -0,0 +1,37 @@
+#Helps accessing the value in the index(multiple) when indexing has limitations
+"""text = "hello world i'm comming for you biatch"
+print(text[32:39:1])
+
+first_name = "Gyanendra"
+second_name = " Aryal"
+print(first_name + second_name)
+
+text = "ahfaiehf fbo9oytboty"
+#print("bot" in text)
+if "bot" in text:
+    print("Bot exists in Text!!");
+if "9oy" in text:
+    print("True it exists")
+else:
+    print("No")"""
+
+#Function in strings
+#.lower():-Lowers the string
+#text = input("Enter any string in Capital: ").lower()
+#print(text);
+#.capitalize():-Capitalizes the first letter of the String
+#name = input("Enter you name in lowercase: ").capitalize()
+#last_name = input("Enter your lastname in small: ").capitalize()
+#fullname = (name + last_name);
+#print(fullname)
+
+#Title:- Capitalizes the every first word in sentence/String.
+#hello world how is everything going on? i am currently watching lost!!
+#text = input("Enter text in small: ").title()
+#print(text)
+
+#swapcase():- As the name suggests it lowers the upper case and upercases the lower one in the string
+#text = input("Enter any text for swapcase: ").swapcase()
+#print(text)
+
+#find():- shows firtst occurance in the string(returns the index/position)
---
+++ Strings/strings.py
@@ -0,0 +1,23 @@
+"""name = "Gyan.py.js"
+name2 = 'python'
+
+name3 = '''
+This is multiline string
+'''
+name4 = """
+#This is also valid multiline string in python
+"""
+print(name,name2,name3,name4);"""
+
+#Indexing
+#right to left -ve indexing
+#left to right +ve indexing
+"""a = "python"
+print(a[4])
+
+for i in a:
+    print(i);"""
+
+#Length of String
+var = "hello world hell yeah"
+print(len(var));
