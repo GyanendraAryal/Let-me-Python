86b0364c7542705211c86265d0a8961d02a96a0a
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-02 14:46:20 +0545

    add basics practice directory

---
+++ BasicsPractice/basic.py
@@ -0,0 +1,17 @@
+#Printing in one line
+# end = " "
+"""name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city")
+print(name, end=" ")
+print(age, end =" ")
+print(city)"""
+
+#Printing pattern
+for i in range(1,5):
+    print("*" * i)
+    print(end ="")
+
+#Calculation
+cal = 5 + 3 * 2
+print(cal)#11 :-PEMDAS
