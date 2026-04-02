37427bf95a665ca3b649ae0cba06c1ed36923473
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-02 14:46:21 +0545

    add operators practice directory

---
+++ OperatorsPractice/operator.py
@@ -0,0 +1,45 @@
+"""a = 5;
+b = 10
+print(b>a);#3True
+
+c = 10
+d = 10
+print(c==d)#True
+
+e = 5
+f = 5
+print(f!=e)#False
+
+num = 17
+num1 = 3
+print(17%3)#2"""
+
+"""number1 = int(input("Enter number: "))
+number2 = int(input("Enter number: "))"""
+"""if number1 % 2==0:
+    print("Number one is even!")
+else:
+    print("Number one is odd!")
+
+if number1 > number2:
+    print("Number one is greater")
+else:
+    print("Number two is greater")"""
+
+number1 = int(input("Enter number: "))
+number2 = int(input("Enter number: "))
+number3 = int(input("Enter number: "))
+
+if number1>number2:
+    large = number1
+    if large > number3:
+        print("Number one is greatest: ", number1)
+elif  number2 > number3:
+    print("Number two is greatest: ", number2)
+else:
+    print("Number third is greatest: ", number3)
+
+
+
+
+
