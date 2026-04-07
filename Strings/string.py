7a7cec3fce87fb508c689d18bb6d02b04c20d0f0
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-07 08:31:56 +0545

    add string.py

---
+++ Strings/string.py
@@ -0,0 +1,13 @@
+chai = 'Lemon Chai'
+# print(chai)
+
+# #first char
+# firstchar = chai[0]
+# print(firstchar)
+
+#Slicing
+slice_val = chai[0:5]#first is inclusive and last is exclusive
+print(slice_val)
+#Slicing all
+slice1_val = chai[:]
+print(slice1_val)
