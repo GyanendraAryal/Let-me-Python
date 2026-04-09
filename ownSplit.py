07f719718a106a31587b4d993406ebb2bee9a56e
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-09 07:44:58 +0545

    custom split function experiment

---
+++ ownSplit.py
@@ -0,0 +1,10 @@
+# a = "38fsjh3453"
+# splitWord = []
+# for i in a:
+#     splitWord.append(i)
+# print(splitWord)
+
+# # #Even Better
+# # a = "23847hgfijsdhg"
+# # splitFunction = list(a)
+# # print(splitFunction)
