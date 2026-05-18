d3a576f6ea5de01bed8c93d05238eccd8861e690
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-18 10:12:16 +0545

    Add polymorphism examples with method overriding

---
+++ OOP/polymorphism.py
@@ -0,0 +1,40 @@
+# Polymorphism - that can have multiple faces or uses
+
+
+class Car:
+    total_car = 0 #To Track the number of cars created using this class
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.__model = model
+        Car.total_car += 1
+
+    def get_brand(self):
+        return f"Brand is {self.__brand}"
+
+    def get_model(self):
+        return f"Model is {self.__model}"
+
+    def fuel_type(self):
+        return f"Fuel type is Diesel or Petrol"
+
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, batterysize):
+        super().__init__(brand, model)
+        self.batterysize = batterysize
+
+    def fuel_type(self):
+        return f"Fuel type is Electric Charge"
+
+
+# myCar = Car("Toyota", "Prado")
+# print(myCar.get_brand())
+# print(myCar.fuel_type())
+
+safari = Car("Tata", "safari")
+print(safari.get_brand())
+print(safari.fuel_type())
+print(safari.total_car) #Why is this worng?
+
+myTesla = ElectricCar("Tesla", "CyberTruckk", "85KWH")
+print(myTesla.fuel_type())
